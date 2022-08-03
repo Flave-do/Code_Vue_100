397d1003c2b6b768f98625fa88b382716dfaf83e
Flave-do <308726757@qq.com>
2022-08-02 19:54:26 -0700

    动画

--- 02DongHua/dong_hua/src/components/HelloWorld.vue
+++ 02DongHua/dong_hua/src/components/HelloWorld.vue
@@ -39,7 +39,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
@@ -52,7 +51,14 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
-a {
-  color: #42b983;
-}
-</style>
+    
+    @keyframes donghuaming{
+		from{
+			transform:translateX(-100px);
+		}
+		
+		to{
+			transform:translateX(0px);
+		}
+	}
+ </style>
