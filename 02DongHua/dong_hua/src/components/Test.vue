77a4fc5b1556c0df0958db9231b2a864f0c14f49
Flave-do <308726757@qq.com>
2022-08-02 19:54:47 -0700

    动画测试

---
+++ 02DongHua/dong_hua/src/components/Test.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <button @click="isShow = !isShow">显示/隐藏</button>
+        <h1 v-show="isShow" class="come">你好啊! </h1>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'Test', 
+        data(){
+            return{
+            	isShow:true
+            }
+        },
+	}
+</script>
+
+<style scoped>
+
+
+    h1{
+    	background-color:orange;
+    }
+    
+    .come{
+    	animation:donghuaming 1s;
+    }
+    
+    .go{ 
+      /* 反转 */
+    	animation:atguigu 1s reverse;
+     
+    }
+    
+    @keyframes donghuaming{
+		from{
+			transform:translateX(-100px);
+		}
+		
+		to{
+			transform:translateX(0px);
+		}
+	}
+ </style>
