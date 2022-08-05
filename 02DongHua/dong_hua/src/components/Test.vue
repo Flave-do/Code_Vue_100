33159d5e8961011ea8afe238253988bb8af0971a
Home <z3407279207@gmail.com>
2022-08-05 17:14:23 +0800

    动画拿来吧

--- 02DongHua/dong_hua/src/components/Test.vue
+++ 02DongHua/dong_hua/src/components/Test.vue
@@ -1,13 +1,20 @@
 <template>
     <div>
         <button @click="isShow = !isShow">显示/隐藏</button>
-        <transition name="hello" :appear="true">
-            <h1 v-show="isShow">你好啊! </h1>    
-        </transition>
+        <transition-group
+        	appear
+        	name="animate__animated animate__bounce"
+        	enter-active-class="animate__swing"
+        	leave-active-class="animate__backOutUp"
+        >
+        	<h1 v-show="!isShow" key="1">你好啊! </h1>
+        	<h1 v-show="isShow" key="2">尚谷! </h1>
+        </transition-group>
     </div>
 </template>
 
 <script>
+    import 'animate.css'
     export default{
         name: 'Test', 
         data(){
@@ -19,8 +26,6 @@
 </script>
 
 <style scoped>
-
-
     h1{
     	background-color:orange;
     }
