03158099a04bb06933e2cfdbcf369bbc0d3da059
Home <z3407279207@gmail.com>
2022-08-08 00:10:49 +0800

    完善等待，真实完善

--- github_users/src/components/List.vue
+++ github_users/src/components/List.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="row">
         <!-- 展示用户列表 -->
-        <div v-show="users.length" class="card" v-for="user in users" :key="user.login">
+        <div v-show="info.users.length" class="card" v-for="user in info.users" :key="user.login">
           <a :href="user.html_url" target="_blank">
             <img :src="user.avatar_url" style='width: 100px'/>
           </a>
           <p class="card-text">{{user.login}}</p>
         </div>
         <!-- 展示欢迎词 -->
-        <h1 v-show="isFirst">欢迎使用! </h1>
+        <h1 v-show="info.isFirst">欢迎使用! </h1>
         <!--展示加载中-->
-        <h1 v-show="isLoading">加载中...</h1>
+        <h1 v-show="info.isLoading">加载中...</h1>
         <!--展示错误信息-->
-        <h1 v-show="errMsg">{{errMsg}}">请求出错了</h1>
+        <h1 v-show="info.errMsg">{{info.errMsg}}">请求出错了</h1>
     </div>
 
 </template>
@@ -22,18 +22,18 @@
         name: ' List',
         data(){
             return{
-                isFirst:true,
-                isLoading:false,
-                errMsg:'',
-                users:[],
+                info:{
+                    isFirst:true,
+                    isLoading:false,
+                    errMsg:'',
+                    users:[],    
+                }
+                
             }
         },
         mounted(){
-            this.$bus.$on('updateListData',(isFirst,isLoading,errMsg,users)=>{
-                this.isFirst = isFirst
-                this.isLoading = isLoading
-                this.errMsg = errMsg
-                this.users = users
+            this.$bus.$on('updatelistData',(dataObj)=>{
+                this.info = dataObj
             })
         }
     }
