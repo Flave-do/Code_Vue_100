6dc90e33027241234fdfc03069fc9710d6c2bb77
Home <z3407279207@gmail.com>
2022-08-07 22:51:01 +0800

    拆组件

--- github_users/src/components/List.vue
+++ github_users/src/components/List.vue
@@ -1,7 +1,36 @@
 <template>
-    <div class="row"> 
-
-    </div> 
+    <div class="row">
+        <div class="card">
+          <a href="https://github.com/xxxxxx" target="_blank">
+            <img src="https://cn.vuejs.org/images/logo.svg" style='width: 100px'/>
+          </a>
+          <p class="card-text">xxxxxx</p>
+        </div>
+        <div class="card">
+          <a href="https://github.com/xxxxxx" target="_blank">
+            <img src="https://cn.vuejs.org/images/logo.svg" style='width: 100px'/>
+          </a>
+          <p class="card-text">xxxxxx</p> 
+        </div>
+        <div class="card">
+          <a href="https://github.com/xxxxxx" target="_blank">
+            <img src="https://cn.vuejs.org/images/logo.svg" style='width: 100px'/> 
+          </a>
+          <p class="card-text">xxxxxx</p>
+        </div>
+        <div class="card">
+          <a href="https://github.com/xxxxxx" target="_blank">
+            <img src="https://cn.vuejs.org/images/logo.svg" style='width: 100px'/> 
+          </a>
+          <p class="card-text">xxxxxx</p>
+        </div>
+        <div class="card">
+          <a href="https://github.com/xxxxxx" target="_blank">
+            <img src="https://cn.vuejs.org/images/logo.svg" style='width: 100px'/> 
+          </a>
+          <p class="card-text">xxxxxx</p>
+        </div>
+    </div>
 
 </template>
 
@@ -9,11 +38,33 @@
     export default{
         name: ' List',
         data(){
-            return
+            return{
+
+            }
         },
     }
 </script>
             
 <style scoped>
-
+  .album{
+    min-height: 50rem; /* Can be removed; just added for demo purposes */ 
+    padding-top: 3rem;
+    padding-bottom: 3rem;
+    background-color: #f7f7f7;
+  }
+  .card{
+    float: Left;
+    width: 33.333%;
+    padding: .75rem;
+    margin-bottom: 2rem;
+    border: 1px solid #efefef;
+    text-align: center;
+  }
+  .card > img {
+    margin-bottom: .75rem;
+    border-radius: 100px;
+  }
+  .card-text{
+    font-size: 85%;
+  }
 </style>
--- github_users/src/components/Search.vue
+++ github_users/src/components/Search.vue
@@ -1,5 +1,10 @@
 <template>
-
+    <section class="jumbotron">
+        <h3 class="jumbotron-heading">Search Github Users</h3>
+        <div>
+            <input type="text" placeholder="enter the name you search"/>&nbsp;<button>Search</button> 
+        </div>
+    </section>
 
 </template>
 
