d0432a84674b867b65858c7196319988325b8206
Home <z3407279207@gmail.com>
2022-08-19 16:15:51 +0800

    更新

---
+++ 05插槽/vue_slot/src/components/Category.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="category">
+        <h3>{{title}}分类</h3>
+        <!-- 定义一个插槽 -->
+        <slot name="center">我是一个默认值，当使用者没有传递时展示</slot>
+        <slot name="footer">我是一个默认值，当使用者没有传递时展示</slot>
+    </div>
+</template>
+<script>
+    export default{
+        name:'Category',
+        props:['title']
+    }
+</script>
+<style>
+    .category{
+        background-color: skyblue;
+        width: 200px;
+        height: 300px;
+    }
+    h3{
+        text-align: center;
+        background-color: orange;
+    }
+</style>
